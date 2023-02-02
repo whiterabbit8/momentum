06dde32175964f18c4e8aceaeb6f844e4c73e012
Natallia <natashaivanyuk@gmail.com>
2023-02-02 20:48:39 +0300

    feat: add functional to accordeon buttons in prices section

--- plants/index.js
+++ plants/index.js
@@ -16,7 +16,7 @@ page.addEventListener('click', function() {
     body.classList.remove('noscroll');
 })
 
-for (let i = 0; i < menuItems.length; i += 1) {
+for (let i = 0; i < menuItems.length; i ++) {
     menuItems[i].addEventListener('click', function() {
         burger.classList.remove('active');
         menu.classList.remove('active');
@@ -42,10 +42,10 @@ srvBtn.forEach(btn => {
         else {
             srvBtn.forEach(b => b.disabled = false)
         }
-        console.log(buttonCounter);
+        /*console.log(buttonCounter);
         console.log(srvBtn[0].disabled === true);
         console.log(srvBtn[1].disabled === true);
-        console.log(srvBtn[2].disabled === true);
+        console.log(srvBtn[2].disabled === true);*/
     })
 })
 
@@ -56,7 +56,7 @@ srvBtn.forEach(btn => {
         }
         else if ((btn.classList.contains('button-active')) && (buttonCounter <= 1)) {
             srvItems.forEach(item => item.classList.add('unactive'))
-            console.log('размываю все')
+            //console.log('размываю все')
         }
     })
 })
@@ -66,7 +66,6 @@ srvBtn[0].addEventListener('click', () => {
         if (item.classList.contains('garden') && (buttonCounter !== 0)) {
             item.classList.toggle('unactive')
         }
-        
     })
 })
 
@@ -85,4 +84,23 @@ srvBtn[2].addEventListener('click', () => {
         }
     })
 })
+
+const accrdBtn = document.querySelectorAll('.accordion-btn');
+const pricesItem = document.querySelectorAll('.prices-item');
+let itemsCount = 0;
+for (let i = 0; i < accrdBtn.length; i++) {
+    accrdBtn[i].addEventListener('click', () => {
+        if (accrdBtn[i].classList.contains('active')) {
+            accrdBtn[i].classList.toggle('active');
+            pricesItem[i].classList.toggle('active');
+        }
+        else {
+            accrdBtn.forEach(btn => btn.classList.remove('active'));
+            pricesItem.forEach(item => item.classList.remove('active'));
+            accrdBtn[i].classList.toggle('active');
+            pricesItem[i].classList.toggle('active');
+        } 
+    })
+}
+
 
