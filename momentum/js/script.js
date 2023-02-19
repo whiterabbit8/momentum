1de5f31cec159bffd9b64105a3c89724a52c573a
Natallia <natashaivanyuk@gmail.com>
2023-02-19 19:23:35 +0300

    feat: add slider for backgrounds

--- momentum/js/script.js
+++ momentum/js/script.js
@@ -1,5 +1,12 @@
 const time = document.querySelector('.time');
 const day = document.querySelector('.date');
+const greeting = document.querySelector('.greeting');
+const userName = document.querySelector('.name');
+const body = document.querySelector('body');
+let randomNum;
+const slideNext = document.querySelector('.slide-next');
+const slidePrev = document.querySelector('.slide-prev');
+
 
 function showTime() {
     const date = new Date();
@@ -19,9 +26,6 @@ function showDate() {
 
 showDate();
 
-
-const greeting = document.querySelector('.greeting');
-
 function getTimeOfDay() {
     const date = new Date();
     const hours = date.getHours();
@@ -31,17 +35,12 @@ function getTimeOfDay() {
     else return 'evening';
 }
 
-const timeOfDay = getTimeOfDay();
-console.log(timeOfDay);
-
 function showGreeting() {
     greeting.textContent = `Good ${getTimeOfDay()}`;
     setTimeout(showGreeting, 1000);
 }
 
 showGreeting();
-
-const userName = document.querySelector('.name');
 
 function setLocalStorage() {
     localStorage.setItem('userName', userName.value);
@@ -56,3 +55,34 @@ function getLocalStorage() {
 }
 
 window.addEventListener('load', getLocalStorage);
+
+function getRandomNum() {
+    randomNum = Math.floor(Math.random() * 20 + 1);
+  }
+
+getRandomNum();
+  console.log(randomNum);
+
+function setBg() {
+    const bgNum = `${randomNum}`.padStart(2, '0');
+    const img = new Image();
+    img.src = `https://raw.githubusercontent.com/whiterabbit8/stage1-tasks/assets/images/${getTimeOfDay()}/` + bgNum + '.jpg';
+    img.onload = () => {
+        body.style.background = `url('https://raw.githubusercontent.com/whiterabbit8/stage1-tasks/assets/images/${getTimeOfDay()}/` + bgNum + ".jpg')";
+    }
+}
+
+setBg();
+
+function getSlideNext() {
+    randomNum < 20 ? randomNum += 1 : randomNum = 1;
+    setBg();
+}
+
+function getSlidePrev() {
+    randomNum !== 1 ? randomNum -= 1 : randomNum = 20;
+    setBg();
+}
+
+slideNext.addEventListener('click', getSlideNext);
+slidePrev.addEventListener('click', getSlidePrev);
